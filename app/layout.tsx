6e92b1f6d564d7f6f20a275bc4b3f745dd44c095
arcanjo.rodrigues <arcanjo.rodrigues@sicoob.com.br>
2025-12-31 20:06:12 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,6 @@ export default function RootLayout({
           defaultTheme="light"
           enableSystem={false}
           disableTransitionOnChange
-          suppressHydrationWarning
       >
         {children}
         <Toaster />
--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
+  output: 'export',
+  outputFileTracingRoot: path.join(__dirname, '../'),
   eslint: {
     ignoreDuringBuilds: true,
   },
